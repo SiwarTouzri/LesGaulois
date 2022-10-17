f9cb45fd546e1640df8d0040c46d4f0c3e4f6dfe
SiwarTouzri <114427757+SiwarTouzri@users.noreply.github.com>
2022-10-17 16:21:59 +0200

    Add files via upload

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,27 +3,40 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetPotion =1;
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-	public int getEffetPotion() {
-		return effetPotion;
-	}
-	public void setEffetPotion(int effetPotion) {
-		this.effetPotion = effetPotion;
-	}
-	public Gaulois(String nom, int force, int effetPotion) {
-		super();
-		this.nom = nom;
-		this.force = force;
-		this.effetPotion = effetPotion;
-	}
+	private int effetpotion = 1;
+	
+
 	public Gaulois(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	}
+
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le gaulois" + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + "envoie un grand coup dans le machoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
+	}
+
+	public static void main(String[] args) {
+		asterix (nom = "Astérix", force = 8);
+		System.out.println(asterix);
+
 	}
 
 }
