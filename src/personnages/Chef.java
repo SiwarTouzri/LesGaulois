15a1b98b1a7632f7b0254256ddc309952042df2d
SiwarTouzri <114427757+SiwarTouzri@users.noreply.github.com>
2022-10-17 17:45:31 +0200

    LesGaulois

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -26,9 +26,9 @@ public class Chef {
 	}
 	
 	public void frapper(Romain romain) {
-	System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
-	romain.getNom());
-	romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+		romain.getNom());
+		romain.recevoirCoup(force / 3);
 	}
 
 
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,38 @@
-package personnages;
-
+package personnages ; 
 public class Village {
+	private String nom ;
+	private Chef chef;
+	private Gaulois []villageois ; 
+	private int nbVillageois ; 
+	private int numvillageois ; 
+	
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	public void setChef(Chef chef , int force ) {
+		this.chef = chef;
+		this.force =6 ;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public Village (int nbVillageoisMaximum , String nom ) {
+		
+		this.nom = nom ; 
+		Gaulois []villageois =new Gaulois [nbVillageoisMaximum] ;
 
+	}
+	public void ajouterHabitant (Gaulois gaulois ) {
+		villageois[nbVillageois]=gaulois ;	
+		nbVillageois=nbVillageois+1  ;
+		
+	}
+	public void trouverHabitant (int numvillageois) {
+		this.numvillageois =numvillageois ;
+		
+	}
+	public void VillagesdesIrreductibles (Village VillagedesIrreductibles  ) {
+		numvillageois =30;
+			
+	}
 }
